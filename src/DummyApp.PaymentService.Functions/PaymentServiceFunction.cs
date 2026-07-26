aff488a5c36741cc2bb9719ecfe2d07118cfec4c
Andrei Shershan <mail.shershan@gmail.com>
2026-07-26 18:11:49 +0200

    Updates and setup

--- src/DummyApp.PaymentService.Functions/PaymentServiceFunction.cs
+++ src/DummyApp.PaymentService.Functions/PaymentServiceFunction.cs
@@ -8,11 +8,11 @@ public sealed class PaymentServiceFunction
 {
     [Function("PaymentService")]
     public HttpResponseData Run(
-#if DEBUG
+// #if DEBUG
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "payment")] HttpRequestData req)
-#else
-        [HttpTrigger(AuthorizationLevel.Function, "get", Route = "payment")] HttpRequestData req)
-#endif
+// #else
+//         [HttpTrigger(AuthorizationLevel.Function, "get", Route = "payment")] HttpRequestData req)
+// #endif
     {
         var response = req.CreateResponse(HttpStatusCode.OK);
         response.WriteString("Hello world");
